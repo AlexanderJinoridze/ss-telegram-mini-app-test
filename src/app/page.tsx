a0337ed3e353b6c353ec35cdae1857ae9c76e63a
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-10-10 09:42:00 +0400

    Add property type selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import {
   LargeTitle,
   Text,
   Input,
-  Badge
+  Badge,
 } from "@telegram-apps/telegram-ui";
 
 import { Link } from "@/components/Link/Link";
@@ -26,6 +26,7 @@ import { ModalHeader } from "@telegram-apps/telegram-ui/dist/components/Overlays
 
 export default function Home() {
   const [dealType, setDealType] = useState(false);
+  const [propertyType, setPropertyType] = useState(false);
 
   return (
     <List>
@@ -49,40 +50,27 @@ export default function Home() {
           </Section>
         </Modal>
       </Section>
-      <Section
-        header="Features"
-        footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-      >
-        <Link href="/ton-connect">
-          <Cell
-            before={
-              <Image src={tonSvg.src} style={{ backgroundColor: "#007AFF" }} />
-            }
-            subtitle="Connect your TON wallet"
-          >
-            TON Connect
-          </Cell>
-        </Link>
-      </Section>
-      <Section
-        header="Application Launch Data"
-        footer="These pages help developer to learn more about current launch information"
-      >
-        <Link href="/init-data">
-          <Cell subtitle="User data, chat information, technical data">
-            Init Data
-          </Cell>
-        </Link>
-        <Link href="/launch-params">
-          <Cell subtitle="Platform identifier, Mini Apps version, etc.">
-            Launch Parameters
-          </Cell>
-        </Link>
-        <Link href="/theme-params">
-          <Cell subtitle="Telegram application palette information">
-            Theme Parameters
-          </Cell>
-        </Link>
+      <Section>
+        <Cell
+          onClick={() => {
+            setPropertyType(!propertyType);
+          }}
+        >
+          ქონების ტიპი
+        </Cell>
+        <Modal open={propertyType}>
+          <ModalHeader />
+          <Text>ქონების ტიპი</Text>
+          <IconButton>გასუფთავება</IconButton>
+          <Section>
+            <Cell>კერძო სახლი</Cell>
+            <Cell>ბინა</Cell>
+            <Cell>მიწა</Cell>
+            <Cell>კომერციული</Cell>
+            <Cell>სასტუმრო</Cell>
+            <Cell>აგარაკი</Cell>
+          </Section>
+        </Modal>
       </Section>
     </List>
   );
