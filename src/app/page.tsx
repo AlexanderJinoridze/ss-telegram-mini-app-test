b5c2e85ba40ebf2a9071f97da4bb0d978891756d
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-10-20 21:51:30 +0400

    Change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ import {
 import { Link } from "@/components/Link/Link";
 
 import tonSvg from "./_assets/ton.svg";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { ModalClose } from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalClose/ModalClose";
 import { ModalHeader } from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader";
 
@@ -30,52 +30,45 @@ export default function Home() {
   const [propertyType, setPropertyType] = useState(false);
 
   return (
-    <List>
-      <Section>
-        <Cell
-          onClick={() => {
-            setDealType(!dealType);
-          }}
-        >
-          გარიგების ტიპი
-        </Cell>
-        <Modal open={dealType}>
-          <ModalHeader />
-          <Text>გარიგების ტიპი</Text>
-          <IconButton>გასუფთავება</IconButton>
-          <Section>
-            <Cell>ქირავდება</Cell>
-            <Cell>იყიდება</Cell>
-            <Cell>გირავდება</Cell>
-            <Cell>ქირავდება დღიურად</Cell>
-          </Section>
-          <Button>ZAZA</Button>
-        </Modal>
+    <Fragment>
+      <List>
+        <Section>
+          <Cell onClick={() => setDealType(!dealType)}>გარიგების ტიპი</Cell>
+          <Cell onClick={() => setPropertyType(!propertyType)}>
+            ქონების ტიპი
+          </Cell>
+        </Section>
+        <Section>
+          <Button mode="filled" stretched>
+            ძიება
+          </Button>
+        </Section>
+      </List>
 
-        <Cell
-          onClick={() => {
-            setPropertyType(!propertyType);
-          }}
-        >
-          ქონების ტიპი
-        </Cell>
-        <Modal open={propertyType}>
-          <ModalHeader />
-          <Text>ქონების ტიპი</Text>
-          <IconButton>გასუფთავება</IconButton>
-          <Section>
-            <ButtonCell>კერძო სახლი</ButtonCell>
-            <ButtonCell>ბინა</ButtonCell>
-            <ButtonCell>მიწა</ButtonCell>
-            <ButtonCell>კომერციული</ButtonCell>
-            <ButtonCell>სასტუმრო</ButtonCell>
-            <ButtonCell>აგარაკი</ButtonCell>
-          </Section>
-        </Modal>
-      </Section>
-      <Button size="l" mode="filled" stretched>
-        ძიება
-      </Button>
-    </List>
+      <Modal open={dealType} modal>
+        <ModalHeader />
+        <Text>გარიგების ტიპი</Text>
+        <IconButton>გასუფთავება</IconButton>
+        <Section>
+          <Cell>ქირავდება</Cell>
+          <Cell>იყიდება</Cell>
+          <Cell>გირავდება</Cell>
+          <Cell>ქირავდება დღიურად</Cell>
+        </Section>
+      </Modal>
+      <Modal open={propertyType} modal>
+        <ModalHeader />
+        <Text>ქონების ტიპი</Text>
+        <IconButton>გასუფთავება</IconButton>
+        <Section>
+          <ButtonCell>კერძო სახლი</ButtonCell>
+          <ButtonCell>ბინა</ButtonCell>
+          <ButtonCell>მიწა</ButtonCell>
+          <ButtonCell>კომერციული</ButtonCell>
+          <ButtonCell>სასტუმრო</ButtonCell>
+          <ButtonCell>აგარაკი</ButtonCell>
+        </Section>
+      </Modal>
+    </Fragment>
   );
 }
