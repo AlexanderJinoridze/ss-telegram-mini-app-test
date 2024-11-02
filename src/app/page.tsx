63f2c0bf3b82075adac63c7fc37e83b49800ff78
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-03 00:02:08 +0400

    Decrease padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -201,7 +201,7 @@ export default function Home() {
           </Section>
           <FixedLayout>
             <Divider />
-            <div className="p-6 bg-[--tg-theme-header-bg-color]">
+            <div className="p-5 bg-[--tg-theme-header-bg-color]">
               <Button size="l" stretched>
                 იპოვე
               </Button>
