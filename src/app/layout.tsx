a71458298661dd0b18bef2dd0b64ac37c4222bef
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-03 00:33:40 +0400

    Remove margin padding from body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=cabin,chair,check,close,home,home_work,hotel,psychiatry"
         />
       </head>
-      <body>
+      <body style={{margin:0, padding: 0}}>
         <Root>{children}</Root>
       </body>
     </html>
