509f64be20ed235b28de8f4127f4e1370bed4f25
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-03 00:53:18 +0400

    Main button text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,8 +137,7 @@ export default function Home() {
               }
               onOpenChange={(open) => {
                 mainButton.setParams({
-                  text: "ZAZA",
-                  isVisible: !open,
+                  text: open ? "არჩევა" : "ძიება",
                 });
                 dealTypeChange();
               }}
