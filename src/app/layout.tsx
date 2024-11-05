7be131e1bcb313ff053fab549a3d3e772f563aa9
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-05 11:44:27 +0400

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({ children }: PropsWithChildren) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=cabin,chair,check,close,home,home_work,hotel,psychiatry"
         />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover"
+        ></meta>
       </head>
       <body className="overflow-hidden absolute h-screen w-screen">
         <Root>{children}</Root>
