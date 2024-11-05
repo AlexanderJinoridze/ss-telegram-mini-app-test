8bb387cfd477de124f6f07d24966e4b00c2835f8
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-05 18:46:19 +0400

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,9 @@ export default function RootLayout({ children }: PropsWithChildren) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=cabin,chair,check,close,home,home_work,hotel,psychiatry"
         />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover"
-        ></meta>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover" />
       </head>
-      <body className="!overflow-hidden !absolute !h-full !w-full">
+      <body className="overflow-hidden absolute h-screen w-screen">
         <Root>{children}</Root>
       </body>
     </html>
