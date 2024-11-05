fa40fb0043a9cd9eb4aba1e42827ddc4cc3f8b8e
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-05 09:41:06 +0400

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -334,7 +334,7 @@ export default function Home() {
                   console.log("PRICE MODAL OPEN CHANGE");
                 }}
                 id="Zaza"
-                className="max-h-[calc(100%-1.5rem)]"
+                className="h-full max-h-[calc(100%-1.5rem)]"
               >
                 <ModalHeader title="ფართი" onClear={propertyTypeClear} />
                 <div className="[&>div]:px-6">
