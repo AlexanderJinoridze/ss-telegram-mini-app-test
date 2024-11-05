bc6181a4ab03fcbd90a087057469542ff11a4b16
Alexander Jinoridze <sanjinoridze@gmail.com>
2024-11-05 22:06:00 +0400

    Layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         />
         {/* <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover" /> */}
       </head>
-      <body>
+      <body className="!relative !overflow-hidden !m-0 !p-0 !flex !flex-col !w-full !h-full">
         <Root>{children}</Root>
       </body>
     </html>
